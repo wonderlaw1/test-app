ee6c59604198607c9b32d63d7f988ec6b796190e
wonderlaw1 <wonderlaw12@gmail.com>
2020-01-07 21:33:14 +0200

    schedule section is in progress

---
+++ src/app/health/schedule/components/schedule-section/schedule-section.component.ts
@@ -0,0 +1,27 @@
+import {ChangeDetectionStrategy, Component, Input, EventEmitter} from '@angular/core';
+
+import {ScheduleItem} from '../../../shared/services/schedule/schedule.service';
+import {Meal} from '../../../shared/services/meals/meals.service';
+import {Workout} from '../../../shared/services/workouts/workouts.service';
+
+@Component({
+  selector: 'app-schedule-section',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  templateUrl: './schedule-section.component.html',
+  styleUrls: ['./schedule-section.component.scss']
+})
+export class ScheduleSectionComponent {
+
+  @Input() name: string;
+  @Input() section: ScheduleItem;
+  @Input() selectSection = new EventEmitter<any>();
+
+  onSelect(type: string, assigned: Meal[] | Workout[] = []) {
+    const data = this.section;
+    this.selectSection.emit({
+      type,
+      assigned,
+      data
+    });
+  }
+}
